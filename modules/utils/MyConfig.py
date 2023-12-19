5ce77045bdbd4697105258fe3fd10cad7333d689
Yusen Wu <sanmusen_wu@outlook.com>
2023-12-19 19:31:41 +0000

    update version

--- modules/utils/MyConfig.py
+++ modules/utils/MyConfig.py
@@ -9,7 +9,7 @@ class MyConfigger:
     
     file_path: config.json的路径
     """
-    NOWVERSION="1.1.0"
+    NOWVERSION="1.1.1beta"
     # 读取config这个py里面的配置
     def __init__(self, file_path=""):
         if file_path != "":
