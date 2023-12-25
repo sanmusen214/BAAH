260c4569c67cb760ff82a61a41bcd29817b6ff6c
Yusen Wu <sanmusen_wu@outlook.com>
2023-12-25 21:44:50 +0000

    chore: gui

--- gui/__init__.py
+++ gui/__init__.py
@@ -397,7 +397,7 @@ def show_GUI(load_jsonname):
                 ui.link_target("WANTED")
                 ui.label('悬赏通缉').style('font-size: x-large')
             
-            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。国服暂未实装扫荡max次')
+            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。')
             list_edit_area(config.configdict["WANTED_HIGHEST_LEVEL"], ["天刷取", "", ["地区", "关卡", "次数"]], "一个月有30天，如果在这里定义了30天每天刷什么，那么每天都会刷取不同的东西。如果定义了3天，那每三天一轮按照这个来刷取。如果只定义1天，那么每天都按照那个刷。\n悬赏通缉的地区就是指进入悬赏通缉页面之后右侧那三个不同的地区（高架公路，沙漠铁道，教室）")
             
             
@@ -405,21 +405,21 @@ def show_GUI(load_jsonname):
                 ui.link_target("SPECIAL_TASK")
                 ui.label('特殊任务/特别委托').style('font-size: x-large')
             
-            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。国服暂未实装扫荡max次')
+            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。')
             list_edit_area(config.configdict["SPECIAL_HIGHTEST_LEVEL"], ["天刷取", "", ["地区", "关卡", "次数"]],"一个月有30天，如果在这里定义了30天每天刷什么，那么每天都会刷取不同的东西。如果定义了3天，那每三天一轮按照这个来刷取。如果只定义1天，那么每天都按照那个刷。\n特殊任务的地区就是指进入页面之后右侧第几个不同的刷取关（经验，金币）")
             
             with ui.row():
                 ui.link_target("EXCHANGE")
                 ui.label('学园交流会').style('font-size: x-large')
             
-            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。国服暂未实装扫荡max次')
+            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。')
             list_edit_area(config.configdict["EXCHANGE_HIGHEST_LEVEL"], ["天刷取", "", ["学院", "关卡", "次数"]],"一个月有30天，如果在这里定义了30天每天刷什么，那么每天都会刷取不同的东西。如果定义了3天，那每三天一轮按照这个来刷取。如果只定义1天，那么每天都按照那个刷。\n学学院就是指进入学园交流会页面之后右侧第几个学院（三一，格黑娜，千年）")
 
             with ui.row():
                 ui.link_target("ACTIVITY")
                 ui.label('活动关卡').style('font-size: x-large')
 
-            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。国服暂未实装扫荡max次')
+            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。')
             list_edit_area(config.configdict["EVENT_QUEST_LEVEL"], ["天刷取", "", ["关卡", "次数"]], "一个月有30天，如果在这里定义了30天每天刷什么，那么每天都会刷取不同的东西。如果定义了3天，那每三天一轮按照这个来刷取。如果只定义1天，那么每天都按照那个刷。")
                 
 
@@ -427,14 +427,14 @@ def show_GUI(load_jsonname):
                 ui.link_target("HARD")
                 ui.label('困难关卡').style('font-size: x-large')
             
-            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。国服暂未实装扫荡max次')
+            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。')
             list_edit_area(config.configdict["HARD"], ["天刷取", "", ["地区", "关卡", "次数"]], "一个月有30天，如果在这里定义了30天每天刷什么，那么每天都会刷取不同的东西。如果定义了3天，那每三天一轮按照这个来刷取。如果只定义1天，那么每天都按照那个刷。")
                 
             with ui.row():
                 ui.link_target("NORMAL")
                 ui.label('普通关卡').style('font-size: x-large')
             
-            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。国服暂未实装扫荡max次')
+            ui.label('关于次数的说明：4次就是扫荡4次，-1次即为扫荡max次，-2次即为扫荡max-2次。')
             list_edit_area(config.configdict["NORMAL"], ["天刷取", "", ["地区", "关卡", "次数"]], "一个月有30天，如果在这里定义了30天每天刷什么，那么每天都会刷取不同的东西。如果定义了3天，那每三天一轮按照这个来刷取。如果只定义1天，那么每天都按照那个刷。")
                 
             with ui.row():
