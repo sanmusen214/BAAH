5be96f76fd3076a268321ecbd8bdab1aa81da714
Yusen Wu <sanmusen_wu@outlook.com>
2024-01-15 15:00:03 +0000

    Feat: suit CollectMails for high render level

--- modules/AllTask/CollectMails/CollectMails.py
+++ modules/AllTask/CollectMails/CollectMails.py
@@ -17,18 +17,24 @@ class CollectMails(Task):
     def pre_condition(self) -> bool:
         return Page.is_page(PageName.PAGE_HOME)
     
-     
+    
     def on_run(self) -> None:
-        self.run_until(
+        if not self.run_until(
             lambda: click((1143, 38)),
             lambda: Page.is_page(PageName.PAGE_MAILBOX)
-        )
+        ):
+            # 如果没到邮箱界面，返回主页
+            return
         click(Page.MAGICPOINT)
         click(Page.MAGICPOINT)
+        if not match(button_pic(ButtonName.BUTTON_ONE_COLLECT), threshold=0.95):
+            # 简单适配下高画质收取邮箱
+            click((1133, 673))
         self.run_until(
             lambda: click(button_pic(ButtonName.BUTTON_ONE_COLLECT)),
             lambda: not match(button_pic(ButtonName.BUTTON_ONE_COLLECT), threshold=0.95)
         )
+        
         self.back_to_home()
 
      
--- modules/AllTask/README.md
+++ modules/AllTask/README.md
@@ -1,6 +1,13 @@
 # Task List
 
 Remember to export DailyTask in `__init__.py`
+记得在__init__.py里导入任务模块
+
 And write the relationship of it in task_dict in `myAllTask.py`
+记得在myAllTask里写明对应关系
 
 `ataskformat.py` is a template for instance of Task
+模板文件ataskformat.py
+
+All task modules named as a folder are all designed to run from the main page.
+所有文件夹命名的任务模块都默认从主页开始运行
