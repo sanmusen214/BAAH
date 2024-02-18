7c86f0f91c6abb12188b31e6f22b198ceb7b08c8
Yusen Wu <sanmusen_wu@outlook.com>
2024-02-18 18:42:59 +0000

    Fix: explore gui can not input 17 cuz 1 is changed

--- gui/pages/Setting_hard.py
+++ gui/pages/Setting_hard.py
@@ -29,6 +29,6 @@ def set_hard(config):
     
     with ui.card():
         ui.checkbox(config.get_text("config_rainbow_teams_desc")).bind_value(config.userconfigdict, "EXPLORE_RAINBOW_TEAMS")
-        ui.number(config.get_text("config_push_hard_desc"), min=0, precision=0, step=1).bind_value(config.userconfigdict, "PUSH_HARD_QUEST", forward=lambda x: int(x)).style("width: 300px")
+        ui.number(config.get_text("config_push_hard_desc"), min=4, precision=0, step=1).bind_value(config.userconfigdict, "PUSH_HARD_QUEST", forward=lambda x: int(x)).style("width: 300px")
         ui.number(config.get_text("config_level"), min=1, precision=0, step=1).bind_value(config.userconfigdict, "PUSH_HARD_QUEST_LEVEL", forward=lambda x:int(x)).style("width: 300px")
     
