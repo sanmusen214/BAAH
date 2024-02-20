fbe138aed425866e03066f7ef7037596b58ed49c
Yusen Wu <sanmusen_wu@outlook.com>
2024-02-20 02:45:28 +0000

    Fix: GUI bugs when add config

--- gui/pages/Setting_notification.py
+++ gui/pages/Setting_notification.py
@@ -8,7 +8,7 @@ def set_notification(config, shared_softwareconfig):
     with ui.card():
         ui.checkbox(config.get_text("email_notification_desc")).bind_value(config.userconfigdict, 'ENABLE_MAIL_NOTI')
         ui.input(config.get_text("email_account")).bind_value(shared_softwareconfig.softwareconfigdict, "MAIL_USER", forward = lambda x:x.replace("@","")).style("width: 300px")
-        ui.input(config.get_text("email_pwd")).bind_value(shared_softwareconfig.softwareconfigdict, "MAIL_PASS").style("width: 300px")
+        ui.input(config.get_text("email_pwd"), password=True).bind_value(shared_softwareconfig.softwareconfigdict, "MAIL_PASS").style("width: 300px")
 
         ui.label(config.get_text("get_email_pwd"))
         ui.html('<iframe src="//player.bilibili.com/player.html?aid=583874363&bvid=BV16z4y1D74s&cid=211611094&p=1&autoplay=0" width="720px" height="480px" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>')
