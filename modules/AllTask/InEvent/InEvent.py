2e9ebd6958c13bd952ffe86cbeeb4507ba9c0232
Yusen Wu <sanmusen_wu@outlook.com>
2024-03-03 14:46:37 +0000

    chore: click magic point before judge page name

--- modules/AllTask/InEvent/InEvent.py
+++ modules/AllTask/InEvent/InEvent.py
@@ -80,7 +80,7 @@ class InEvent(Task):
         if not check_app_running(config.userconfigdict['ACTIVITY_PATH']):
             logging.warn("跳转出了游戏，尝试重新进入游戏")
             open_app(config.userconfigdict['ACTIVITY_PATH'])
-            sleep(1)
+            sleep(3)
             if not check_app_running(config.userconfigdict['ACTIVITY_PATH']):
                 logging.error("重新进入游戏失败")
                 raise Exception("重新进入游戏失败")
@@ -88,11 +88,12 @@ class InEvent(Task):
         if not Page.is_page(PageName.PAGE_EVENT):
             # 可能首次进入活动，有活动剧情
             SkipStory(pre_times=5).run()
+        click(Page.MAGICPOINT, sleeptime=0.3)
+        click(Page.MAGICPOINT, sleeptime=0.3)
+        screenshot()
         # 判断左上角标题
         if not Page.is_page(PageName.PAGE_EVENT):
             return False
-        click(Page.MAGICPOINT, sleeptime=0.3)
-        click(Page.MAGICPOINT, sleeptime=0.3)
         # 图片匹配深色的QUEST标签
         matchpic = self.run_until(
             lambda: click((965, 98)),
