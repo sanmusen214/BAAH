0848105b98431ec13b93cf9240e0af41fc6f780f
Yusen Wu <sanmusen_wu@outlook.com>
2024-04-03 14:04:03 +0100

    update: GUI

--- gui/pages/Setting_event.py
+++ gui/pages/Setting_event.py
@@ -9,7 +9,9 @@ def set_event(config):
     
     ui.checkbox(config.get_text("config_auto_event_story")).bind_value(config.userconfigdict, "AUTO_EVENT_STORY_PUSH")
     
-    ui.checkbox(config.get_text("config_auto_event_push")).bind_value(config.userconfigdict, "AUTO_PUSH_EVENT_QUEST")
+    with ui.row():
+        ui.checkbox(config.get_text("config_auto_event_push")).bind_value(config.userconfigdict, "AUTO_PUSH_EVENT_QUEST")
+        ui.checkbox(config.get_text("raise_error_if_can_not_push_event_level")).bind_value(config.userconfigdict, "RAISE_ERROR_IF_CANNOT_PUSH_EVENT_QUEST").bind_visibility_from(config.userconfigdict, "AUTO_PUSH_EVENT_QUEST")
     
     ui.label(config.get_text("config_desc_times"))
     list_edit_area(config.userconfigdict["EVENT_QUEST_LEVEL"], [config.get_text("config_day"), "", [config.get_text("config_level"), config.get_text("config_times")]], config.get_text("config_desc_list_edit"))
