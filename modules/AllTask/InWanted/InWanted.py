da18742c0caf62c1435a65e16f4946e4c812f3bc
Liu Yan <liu5519@icloud.com>
2024-06-24 04:05:29 +0800

    Feat(InWanted): English localization

--- modules/AllTask/InWanted/InWanted.py
+++ modules/AllTask/InWanted/InWanted.py
@@ -1,5 +1,3 @@
- 
-
 from DATA.assets.PageName import PageName
 from DATA.assets.ButtonName import ButtonName
 from DATA.assets.PopupName import PopupName
@@ -17,26 +15,27 @@ from modules.configs.MyConfig import config
 class InWanted(Task):
     def __init__(self, name="InWanted") -> None:
         super().__init__(name)
-        
 
-     
+
+
     def pre_condition(self) -> bool:
         if len(config.userconfigdict['WANTED_HIGHEST_LEVEL']) == 0:
-            logging.warn({"zh_CN": "没有配置悬赏通缉的level", "en_US":"There is no level of wanted"})
+            logging.warn({"zh_CN": "没有配置悬赏通缉的level", "en_US" :"There is no level of wanted"})
             return False
         return Page.is_page(PageName.PAGE_HOME)
-    
-     
+
+
     def on_run(self) -> None:
         # 得到今天是几号
         today = time.localtime().tm_mday
         # 选择一个location的下标
-        target_loc = today%len(config.userconfigdict['WANTED_HIGHEST_LEVEL'])
+        target_loc = today % len(config.userconfigdict['WANTED_HIGHEST_LEVEL'])
         target_info = config.userconfigdict['WANTED_HIGHEST_LEVEL'][target_loc]
         # 这之后target_info是一个list，内部会有多个关卡扫荡
         # 判断这一天是否设置有扫荡任务关卡
         if len(target_info) == 0:
-            logging.warn({"zh_CN": "今天轮次中无悬赏通缉关卡，跳过", "en_US":"There is no wanted level in today's round, skip"})
+            logging.warn({"zh_CN": "今天轮次中无悬赏通缉关卡，跳过",
+                          "en_US": "There is no wanted level in today's round, skip"})
             return
         # 序号转下标
         target_info = [[each[0]-1, each[1]-1, each[2]] for each in target_info]
@@ -52,16 +51,16 @@ class InWanted(Task):
             lambda: Page.is_page(PageName.PAGE_WANTED),
         )
         if not caninwanted:
-            logging.warning("Can't open wanted page, task quit")
+            logging.warn({"zh_CN": "无法打开通缉页面，任务退出", "en_US": "Can't open wanted page, task quit"})
             self.back_to_home()
             return
         # 开始循环扫荡target_info中的每一个关卡
         for each_target in target_info:
             # check whether there is a ticket
             # 使用PageName.PAGE_WANTED的坐标判断是国服还是其他服
-            if match(page_pic(PageName.PAGE_WANTED), returnpos=True)[1][1]>133:
+            if match(page_pic(PageName.PAGE_WANTED), returnpos=True)[1][1] > 133:
                 # 如果右侧Title较低，说明是老版本的国服
-                logging.info("点击较低的三个定位点")
+                logging.info({"zh_CN": "点击较低的三个定位点", "en_US": "Click on the lower three anchors"})
                 points = np.linspace(265, 544, 3)
             else:
                 # 可点击的一列点
@@ -79,9 +78,8 @@ class InWanted(Task):
                 lambda: not Page.is_page(PageName.PAGE_WANTED_SUB),
                 sleeptime=3
             )
-                
+
         self.back_to_home()
 
-     
     def post_condition(self) -> bool:
         return Page.is_page(PageName.PAGE_HOME)
