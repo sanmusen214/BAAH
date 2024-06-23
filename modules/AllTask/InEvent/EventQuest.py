e5777b65c1ac8a3e6085f8439ae0f51c9459c116
Liu Yan <liu5519@icloud.com>
2024-06-24 00:35:02 +0800

    Feat(InEvent): English localization

--- modules/AllTask/InEvent/EventQuest.py
+++ modules/AllTask/InEvent/EventQuest.py
@@ -1,4 +1,3 @@
- 
 from modules.utils.log_utils import logging
 import time
 from modules.AllTask.SubTask.RaidQuest import RaidQuest
@@ -16,8 +15,9 @@ from modules.AllTask.Task import Task
 
 from modules.utils import click, swipe, match, page_pic, button_pic, popup_pic, sleep, ocr_area, match_pixel, screenshot
 
+
 class EventQuest(Task):
-    def __init__(self, level_list, explore = True, raid = True, collect = True, name="EventQuest") -> None:
+    def __init__(self, level_list, explore=True, raid=True, collect=True, name="EventQuest") -> None:
         super().__init__(name)
         self.level_list = level_list
         # 记录上次自动推图的关卡下标
@@ -29,34 +29,38 @@ class EventQuest(Task):
         # 是否领取奖励
         self.collect = collect
 
-     
     def pre_condition(self) -> bool:
         return Page.is_page(PageName.PAGE_EVENT)
-    
+
     def judge_whether_and_do_fight(self, this_level_ind):
         """
         观察下标为this_level_ind的关卡是否可扫荡，如果不可扫荡就尝试推一次
-        
+
         返回是否尝试了推一次
         """
         if not config.userconfigdict["AUTO_PUSH_EVENT_QUEST"] or not self.explore:
             return "no"
         screenshot()
         if not match(popup_pic(PopupName.POPUP_TASK_INFO)) and not match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE):
-            logging.info("触发推图任务")
+            logging.info({"zh_CN": "触发推图任务", "en_US": "Triggering a tweet task"})
             # 判断推图是否刚才打了一次，但是没三星或打不过去
             if this_level_ind == self.last_fight_level_ind:
                 if config.userconfigdict["RAISE_ERROR_IF_CANNOT_PUSH_EVENT_QUEST"]:
-                    raise Exception(f"活动自动推图第{this_level_ind+1}关刚才打了一次，但是没三星或打不过去，请配置更好的队伍配置")
+                    raise Exception(
+                        f"活动自动推图第{this_level_ind + 1}关刚才打了一次，但是没三星或打不过去，请配置更好的队伍配置")
                 else:
-                    logging.warn({"zh_CN": f"活动自动推图第{this_level_ind+1}关刚才打了一次，但是没三星或打不过去，请配置更好的队伍配置", "en_US":f"The {this_level_ind+1}th level of the event automatic push map was just played once, but it did not get three stars or could not pass, please configure a better team configuration"})
+                    logging.warn({"zh_CN": f"活动自动推图第{this_level_ind + 1}关刚才打了一次，但是没三星或打不过去，请配置更好的队伍配置",
+                                  "en_US": f"The {this_level_ind + 1}th level of the event automatic push map "
+                                           f"was just played once, "
+                                           f"but it did not get three stars or could not pass, "
+                                           f"please configure a better team configuration"})
                     return "noap"
             # 点击任务开始按钮
             click(button_pic(ButtonName.BUTTON_TASK_START))
             # 如果体力不够
             screenshot()
             if match(popup_pic(PopupName.POPUP_TOTAL_PRICE)):
-                logging.warn({"zh_CN": "体力不够，结束", "en_US":"Not enough AP, end"})
+                logging.warn({"zh_CN": "体力不够，结束", "en_US": "Not enough AP, end"})
                 return "noap"
             # 单次战斗
             FightQuest(backtopic=lambda: match(page_pic(PageName.PAGE_EVENT))).run()
@@ -64,7 +68,7 @@ class EventQuest(Task):
             self.last_fight_level_ind = this_level_ind
             return "yes"
         return "no"
-    
+
     def try_collect_all_rewards(self):
         """尝试领取右下角蓝色奖励资讯和左下角每日任务奖励，调用此函数时确保在Quest栏，函数结束会返回到活动页面"""
         self.run_until(
@@ -72,15 +76,16 @@ class EventQuest(Task):
             lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE)
         )
         if not self.collect:
-            logging.info("不尝试领取奖励")
+            logging.info({"zh_CN": "不尝试领取奖励", "en_US": "Don't try to claim your reward"})
             return
         else:
-            logging.info("尝试领取右下角点数奖励和左下角任务奖励")
+            logging.info({"zh_CN": "尝试领取右下角点数奖励和左下角任务奖励",
+                          "en_US": "Try to claim the Bottom Right Point Reward and the Bottom Left Quest Reward"})
         # 领取右下
         self.run_until(
             lambda: click((1124, 659)),
-            lambda: not match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE), # 弹窗
-            times = 3
+            lambda: not match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE),  # 弹窗
+            times=3
         )
         # 通过点中间偏左，防止万一点到关卡的开始任务按钮
         click((585, 603))
@@ -92,7 +97,8 @@ class EventQuest(Task):
         )
         # 领取活动任务
         if match(button_pic(ButtonName.BUTTON_EVENT_DAILY_TASK)):
-            logging.info("检测到活动任务页面，尝试领取任务奖励")
+            logging.info({"zh_CN": "检测到活动任务页面，尝试领取任务奖励",
+                          "en_US": "Campaign quest page detected, try to claim quest rewards"})
             self.run_until(
                 lambda: click(button_pic(ButtonName.BUTTON_EVENT_DAILY_TASK)),
                 lambda: not Page.is_page(PageName.PAGE_EVENT)
@@ -118,15 +124,13 @@ class EventQuest(Task):
             times=3,
             sleeptime=2
         )
-            
-            
-    
+
     def on_run(self) -> None:
         # 按level执行
         for level in self.level_list:
             level_ind = level[0]
             repeat_times = level[1]
-            while(1):
+            while True:
                 # 可能触发推图任务，触发推图的话就重新滑到顶部再往右选
                 sleep(3)
                 # 视角会自动滚动到顶部，等3秒
@@ -136,38 +140,41 @@ class EventQuest(Task):
                 self.scroll_right_up()
                 # 点击第一个level
                 click((1130, 200), sleeptime=2)
-                logging.info(f"尝试跳转到第{level_ind+1}个level")
+                logging.info({"zh_CN": f"尝试跳转到第{level_ind+1}个level",
+                              "en_US": f"Try to jump to the {level_ind+1} level"})
                 # 向右挪到第level_ind个level
                 if not config.userconfigdict["AUTO_PUSH_EVENT_QUEST"] or not self.explore:
-                    logging.info("设置的不自动推活动图，尝试直接跳转到扫荡关卡")
+                    logging.info({"zh_CN": "设置的不自动推活动图，尝试直接跳转到扫荡关卡",
+                                  "en_US": "Set to not push the activity diagram automatically, "
+                                           "try to jump directly to the sweep level"})
                 # 判断是否要推图并推图
-                res=self.judge_whether_and_do_fight(0)
-                if res=="yes":
+                res = self.judge_whether_and_do_fight(0)
+                if res == "yes":
                     # 继续从头滑动
                     continue
-                elif res=="noap":
+                elif res == "noap":
                     self.run_until(
                         lambda: click(Page.MAGICPOINT),
                         lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE)
                     )
-                    logging.info("返回到根页面")
+                    logging.info({"zh_CN": "返回到根页面", "en_US": "Back to Root Page"})
                     self.try_collect_all_rewards()
                     return
                 hasfight_newlevel = False
                 for i in range(level_ind):
                     click((1171, 359), sleeptime=1)
                     # 判断是否要推图并推图
-                    res=self.judge_whether_and_do_fight(1+i)
-                    if res=="yes":
+                    res = self.judge_whether_and_do_fight(1 + i)
+                    if res == "yes":
                         # 设置继续从头滑动
                         hasfight_newlevel = True
                         break
-                    elif res=="noap":
+                    elif res == "noap":
                         self.run_until(
                             lambda: click(Page.MAGICPOINT),
                             lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE)
                         )
-                        logging.info("返回到根页面")
+                        logging.info({"zh_CN": "返回到根页面", "en_US": "Back to Root Page"})
                         self.try_collect_all_rewards()
                         return
                 if hasfight_newlevel:
@@ -175,10 +182,10 @@ class EventQuest(Task):
                     continue
                 # 结束自动推图，或不用推图：扫荡
                 if self.raid:
-                    logging.info("开始扫荡")
+                    logging.info({"zh_CN": "开始扫荡", "en_US": "Start Auto-clear"})
                     RaidQuest(repeat_times).run()
                 # 关闭任务咨询弹窗
-                logging.info("关闭任务咨询弹窗")
+                logging.info({"zh_CN": "关闭任务咨询弹窗", "en_US": "Close task inquiry popup"})
                 Task.run_until(
                     lambda: click(Page.MAGICPOINT),
                     lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE)
@@ -189,8 +196,6 @@ class EventQuest(Task):
             lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE)
         )
         self.try_collect_all_rewards()
-        
 
-     
     def post_condition(self) -> bool:
         return Page.is_page(PageName.PAGE_EVENT)
--- modules/AllTask/InEvent/EventStory.py
+++ modules/AllTask/InEvent/EventStory.py
@@ -1,4 +1,3 @@
- 
 from modules.utils.log_utils import logging
 
 from DATA.assets.PageName import PageName
@@ -10,45 +9,46 @@ from modules.AllTask.SubTask.FightQuest import FightQuest
 from modules.AllPage.Page import Page
 from modules.AllTask.Task import Task
 
-from modules.utils import click, swipe, match, page_pic, button_pic, popup_pic, sleep, ocr_area, config, screenshot, match_pixel
+from modules.utils import (click, swipe, match, page_pic, button_pic, popup_pic, sleep, ocr_area, config, screenshot,
+                           match_pixel)
+
 
 class EventStory(Task):
     """
     判断活动剧情是否推完，如果没推完就推完
-    
+
     靠蒙版是否存在判断剧情是否看过，默认有9关剧情
     """
+
     def __init__(self, name="EventStory") -> None:
         super().__init__(name)
         self.last_fight_level_ind = -1
         # 最大关卡数，这里不是下标，是关卡序号
         self.max_level = 9
 
-     
     def pre_condition(self) -> bool:
         return Page.is_page(PageName.PAGE_EVENT)
-    
-    
+
     def do_view(self):
         """弹窗已经打开，观看当前页面的剧情，可能要打架"""
         self.run_until(
             lambda: click(button_pic(ButtonName.BUTTON_TASK_START)) or click((633, 496)),
             lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE) or match(popup_pic(PopupName.POPUP_TOTAL_PRICE)),
-            times = 3, 
+            times=3,
             sleeptime=2
         )
         # 如果体力不够
         screenshot()
         if match(popup_pic(PopupName.POPUP_TOTAL_PRICE)):
-            logging.warn({"zh_CN": "体力不够，结束", "en_US":"Not enough AP, end"})
+            logging.warn({"zh_CN": "体力不够，结束", "en_US": "Not enough AP, end"})
             return "noap"
         # 剧情这边应该只会有单次战斗
         FightQuest(backtopic=lambda: match(page_pic(PageName.PAGE_EVENT))).run()
-    
+
     def judge_whether_and_do_view(self, this_level_ind):
         """
         判断并执行观看Story，可能要打架
-        
+
         观察到下标为this_level_ind的关卡时蒙版是否还在
         """
         screenshot()
@@ -57,15 +57,22 @@ class EventStory(Task):
         if match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE):
             # 跑到下标为this_level_ind的关卡时蒙版消失
             # 所以我们应该打下标为this_level_ind-1的关卡
-            logging.info(f"触发推剧情任务，关卡{this_level_ind}")
+            logging.info({"zh_CN": f"触发推剧情任务，关卡{this_level_ind}",
+                          "en_US": f"Trigger push story quests, levels {this_level_ind}"})
             # 判断推图是否刚才打了一次，但是没三星或打不过去
             if this_level_ind == self.last_fight_level_ind:
-                logging.warn({"zh_CN": f"活动剧情推图第{this_level_ind}关刚才打了一次，但是没三星或打不过去，请配置更好的队伍配置。或已经是最后一关了", "en_US":f"The {this_level_ind}th level of the event story automatic push map was just played once, but it did not get three stars or could not pass, please configure a better team configuration. Or it is the last level already"})
+                logging.warn({"zh_CN": f"活动剧情推图第{this_level_ind}关刚才打了一次，但是没三星或打不过去，"
+                                       f"请配置更好的队伍配置。或已经是最后一关了",
+                              "en_US": f"The {this_level_ind}th level of the event story "
+                                       f"automatic push map was just played once, "
+                                       f"but it did not get three stars or could not pass, "
+                                       f"please configure a better team configuration. "
+                                       f"Or it is the last level already"})
                 return "repeatfight"
             # 这里弹窗已经关了，重新跑到下标为this_level_ind-1的关卡
             self.scroll_right_up()
             click((1130, 200), sleeptime=2)
-            for i in range(this_level_ind-1):
+            for i in range(this_level_ind - 1):
                 # 点右边的箭头
                 click((1171, 359), sleeptime=1)
             self.do_view()
@@ -73,11 +80,11 @@ class EventStory(Task):
             self.last_fight_level_ind = this_level_ind
             return "yes"
         return "no"
-    
+
     def get_biggest_level(self):
         """
         通过下滑到底ocr获取最大关卡数
-        
+
         -1表示没有找到
         """
         self.scroll_right_down()
@@ -85,7 +92,7 @@ class EventStory(Task):
         screenshot()
         reslist = ocr_area((695, 416), (752, 699), multi_lines=True)
         temp_max = -1
-        logging.info("ocr结果："+str(reslist))
+        logging.info({"zh_CN": "ocr结果：" + str(reslist), "en_US": "ocr result: " + str(reslist)})
         # 将每一个字母尝试转换成数字，如果是数字就比较目前最大
         for res in reslist:
             try:
@@ -101,9 +108,11 @@ class EventStory(Task):
         click((766, 98))
         max_level = self.get_biggest_level()
         if max_level == -1:
-            logging.warn({"zh_CN": "无法获取最大关卡数，结束", "en_US":"Failed to get the max level, end"})
+            logging.warn({"zh_CN": "无法获取最大关卡数，结束", "en_US": "Failed to get the max level, end"})
             return
-        logging.info("检查活动剧情是否推完,最大关卡数为"+str(max_level))
+        logging.info({"zh_CN": "检查活动剧情是否推完,最大关卡数为"+str(max_level),
+                      "en_US": "Check if the event story is completed. "
+                               "The maximum number of levels is" + str(max_level)})
         while 1:
             sleep(3)
             # 视角会自动滚动到顶部，等3秒
@@ -117,23 +126,23 @@ class EventStory(Task):
             # 往右切换，主要是靠break，循环次数只要大于9就行
             has_do_view = False
             # 稍微设置大一点，让重复打关触发来判定结束
-            for i in range(max_level-1):
+            for i in range(max_level - 1):
                 # 点右边的箭头
                 click((1171, 359), sleeptime=1)
                 # 往右切换后判断是否需要推剧情
-                res=self.judge_whether_and_do_view(1+i)
-                if res=="yes":
-                    has_do_view=True
+                res = self.judge_whether_and_do_view(1 + i)
+                if res == "yes":
+                    has_do_view = True
                     break
-                elif res=="noap":
+                elif res == "noap":
                     self.run_until(
                         lambda: click(Page.MAGICPOINT),
                         lambda: match_pixel(Page.MAGICPOINT, Page.COLOR_WHITE)
                     )
-                    logging.info("返回到根页面")
+                    logging.info({"zh_CN": "返回到根页面", "en_US": "Back to Root Page"})
                     return
-                elif res=="repeatfight":
-                    logging.warn({"zh_CN": "重复打关卡，结束", "en_US":"Repeatedly play the level, end"})
+                elif res == "repeatfight":
+                    logging.warn({"zh_CN": "重复打关卡，结束", "en_US": "Repeatedly play the level, end"})
                     return
             # 观看了剧情，那么再尝试继续看后面的关卡剧情
             if has_do_view:
@@ -142,12 +151,13 @@ class EventStory(Task):
             # 不过最后一个剧情无法通过弹窗的方式检测，最后一个剧情用奖励文字检测有没有打过
             screenshot()
             if match(button_pic(ButtonName.BUTTON_EVENT_FIRST)):
-                logging.info("需要推最后一关剧情")
+                logging.info({"zh_CN": "需要推最后一关剧情",
+                              "en_US": "Need to push the last stage of the plot"})
                 self.do_view()
             # 如果没有需要看的剧情了，那么就结束
             click(Page.MAGICPOINT)
             click(Page.MAGICPOINT)
             break
-     
+
     def post_condition(self) -> bool:
         return Page.is_page(PageName.PAGE_EVENT)
