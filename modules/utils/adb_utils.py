5219e988cb56998968e84244e1bd1627a01b6a1e
Liu Yan <liu5519@icloud.com>
2024-06-24 04:09:32 +0800

    Feat(utils): English localization

--- modules/utils/adb_utils.py
+++ modules/utils/adb_utils.py
@@ -6,23 +6,26 @@ import time
 import numpy as np
 import cv2
 
+
 def getNewestSeialNumber(use_config=None):
     # 如果传入指定的配置文件，就使用指定的配置文件
     target_config = config
     if use_config:
         target_config = use_config
-    
+
     if target_config.userconfigdict["ADB_DIRECT_USE_SERIAL_NUMBER"]:
         # 得到完整的序列号，emulator-5554
         return target_config.userconfigdict["ADB_SEIAL_NUMBER"]
     elif target_config.userconfigdict["TARGET_PORT"] and target_config.userconfigdict["TARGET_IP_PATH"]:
         # 从配置文件里得到模拟器IP和端口
-        return "{}:{}".format(target_config.userconfigdict["TARGET_IP_PATH"],target_config.userconfigdict["TARGET_PORT"])
+        return "{}:{}".format(target_config.userconfigdict["TARGET_IP_PATH"],
+                              target_config.userconfigdict["TARGET_PORT"])
     else:
-        logging.error({"zh_CN": "TARGET_IP_PATH或TARGET_PORT未设置", "en_US":"TARGET_IP_PATH or TARGET_PORT not set"})
-        logging.warn({"zh_CN": "使用默认值：127.0.0.1:5555", "en_US":"127.0.0.1:5555 is used as default value"})
+        logging.error({"zh_CN": "TARGET_IP_PATH或TARGET_PORT未设置", "en_US": "TARGET_IP_PATH or TARGET_PORT not set"})
+        logging.warn({"zh_CN": "使用默认值：127.0.0.1:5555", "en_US": "127.0.0.1:5555 is used as default value"})
         return "127.0.0.1:5555"
-    
+
+
 def get_config_adb_path(use_config=None):
     target_config = config
     # 如果传入指定的配置文件，就使用指定的配置文件
@@ -30,14 +33,17 @@ def get_config_adb_path(use_config=None):
         target_config = use_config
     return target_config.userconfigdict['ADB_PATH']
 
+
 # 判断是否有TARGET_PORT这个配置项
 def disconnect_this_device():
     """Disconnect this device."""
     subprocess_run([get_config_adb_path(), "disconnect", getNewestSeialNumber()])
-    
+
+
 def kill_adb_server():
     """Kill the adb server."""
     subprocess_run([get_config_adb_path(), "kill-server"])
+
 
 def connect_to_device(use_config=None):
     """Connect to a device with the given device port."""
@@ -45,7 +51,8 @@ def connect_to_device(use_config=None):
         subprocess_run([get_config_adb_path(use_config), "connect", getNewestSeialNumber(use_config)])
     else:
         subprocess_run([get_config_adb_path(), "connect", getNewestSeialNumber()])
-    
+
+
 def click_on_screen(x, y):
     """Click on the given coordinates."""
     subprocess_run([get_config_adb_path(), "-s", getNewestSeialNumber(), "shell", "input", "tap", str(int(x)), str(int(y))])
@@ -57,9 +64,10 @@ def swipe_on_screen(x1, y1, x2, y2, ms):
 def convert_img(path):
     with open(path, "rb") as f:
         bys = f.read()
-        bys_ = bys.replace(b"\r\n",b"\n")  # 二进制流中的"\r\n" 替换为"\n"
+        bys_ = bys.replace(b"\r\n", b"\n")  # 二进制流中的"\r\n" 替换为"\n"
     with open(path, "wb") as f:
         f.write(bys_)
+
 
 def screen_shot_to_global(use_config=None):
     """Take a screenshot and save it to the GlobalState."""
@@ -72,9 +80,9 @@ def screen_shot_to_global(use_config=None):
        subprocess_run([get_config_adb_path(target_config), "-s", getNewestSeialNumber(target_config), "shell", "screencap", "-p"], stdout=out)
     #adb 命令有时直接截图保存到电脑出错的解决办法-加下面一段即可
     convert_img("./{}".format(filename))
-    
+
     # 方法二，使用cv2提取PIPE管道中的数据
-    
+
     # # 使用subprocess的Popen调用adb shell命令，并将结果保存到PIPE管道中
     # process = subprocess.run([get_config_adb_path(), "-s", getNewestSeialNumber(), "shell", "screencap", "-p"], stdout=subprocess.PIPE)
     # # 读取管道中的数据
@@ -84,7 +92,8 @@ def screen_shot_to_global(use_config=None):
     # # 使用numpy和imdecode将二进制数据转换成cv2的mat图片格式
     # img_screenshot = cv2.imdecode(np.frombuffer(binary_screenshot, np.uint8), cv2.IMREAD_COLOR)
     # cv2.imwrite("./{}".format(config.userconfigdict['SCREENSHOT_NAME']), img_screenshot)
-    
+
+
 def get_now_running_app(use_config=None):
     """
     获取当前运行的app的前台activity
@@ -99,25 +108,29 @@ def get_now_running_app(use_config=None):
             # 找到当前运行的app那行
             output = sentence
             if "null" in output:
-                logging.warn({"zh_CN": "MUMU模拟器需要设置里关闭保活！", "en_US":"If you are using MUMU emulator, please turn off the keep alive in the settings!"})
+                logging.warn({"zh_CN": "MUMU模拟器需要设置里关闭保活！",
+                              "en_US": "If you are using MUMU emulator, please turn off the keep alive in the settings!"})
             break
     # 截取app activity
     try:
         app_activity = output.split(" ")[-1].split("}")[0]
     except Exception as e:
-        logging.warn({"zh_CN": "截取当前运行的app名失败：{}".format(output), "en_US":"Failed to get the current running app name:{}".format(output)})
+        logging.warn({"zh_CN": "截取当前运行的app名失败：{}".format(output),
+                      "en_US": "Failed to get the current running app name:{}".format(output)})
         return output
     return app_activity
+
 
 def get_now_running_app_entrance_activity(use_config=None):
     """
     得到当前app的入口activity
-    
+
     https://stackoverflow.com/questions/12698814/get-launchable-activity-name-of-package-from-adb/41325792#41325792
     """
     # 先获取当前运行的app的前台activity
     front_activity = get_now_running_app(use_config)
-    logging.info("当前运行的app的前台activity是：{}".format(front_activity))
+    logging.info({"zh_CN": "当前运行的app的前台activity是：{}".format(front_activity),
+                  "en_US": "The foreground activity of the currently running app is: {}" .format (front_activity)})
     # 提取出包名
     package_name = front_activity.split("/")[0]
     if use_config:
@@ -128,28 +141,32 @@ def get_now_running_app_entrance_activity(use_config=None):
     strlist = output.split()
     entrance_activity = strlist[-1]
     if "/" not in entrance_activity:
-        logging.error({"zh_CN": f"获取入口activity失败：{output}", "en_US":f"Failing to get entrance activity: {output}"})
+        logging.error({"zh_CN": f"获取入口activity失败：{output}",
+                       "en_US":f"Failing to get entrance activity: {output}"})
         return entrance_activity
     return entrance_activity
 
-def check_app_running(activity_path:str) -> bool:
+
+def check_app_running(activity_path: str) -> bool:
     """
     检查app是否在运行，不校验app的activity,只校验app的名字
     """
     try:
         app_name = activity_path.split("/")[0]
     except Exception as e:
-        logging.error({"zh_CN": "activity_path格式错误", "en_US":"The format of activity_path is wrong"})
+        logging.error({"zh_CN": "activity_path格式错误", "en_US": "The format of activity_path is wrong"})
         return False
     # 获取当前运行的app
     output = get_now_running_app()
-    logging.info("当前运行的app是：{}".format(output))
+    logging.info({"zh_CN": "当前运行的app是：{}".format(output),
+                  "en_US": "The currently running app is: {}".format(output)})
     if app_name in output:
         return True
     else:
         return False
-    
-def open_app(activity_path:str):
+
+
+def open_app(activity_path: str):
     """
     使用adb打开app
     """
