29a47c3711c3e6669cbdee9523bf17c74a274ceb
Yusen Wu <sanmusen_wu@outlook.com>
2024-08-03 22:39:27 +0100

    Update: use element refresh but not middleware

--- jsoneditor.py
+++ jsoneditor.py
@@ -19,66 +19,19 @@ if __name__ in {"__main__", "__mp_main__"}:
         from gui import show_GUI
         from gui.components.check_update import only_check_version
         from nicegui import native, ui, run, app
-
-        from typing import Optional
-
-        from fastapi import Request
-        from fastapi.responses import RedirectResponse
-        from starlette.middleware.base import BaseHTTPMiddleware
-
+        
         import argparse
 
-        unrestricted_page_routes = {'/login'}
-        
+        # 解析器
         parser = argparse.ArgumentParser()
         parser.add_argument("--host", help="host address", default="127.0.0.1")
-        parser.add_argument("--auth", help="enable authentication, only for web", action="store_true")
-        parser.add_argument("--username", help="username", default="admin")
-        parser.add_argument("--password", help="password", default="admin")
+        parser.add_argument("--port", help="host port", default=native.find_open_port())
+        parser.add_argument("--token", help="password", default=None)
         args = parser.parse_args()
 
-        passwords = {
-            args.username: args.password,
-        }
-        
-
-        class AuthMiddleware(BaseHTTPMiddleware):
-            """This middleware restricts access to all NiceGUI pages.
-
-            It redirects the user to the login page if they are not authenticated.
-            """
-
-            async def dispatch(self, request: Request, call_next):
-                if not app.storage.user.get('authenticated', False):
-                    if not request.url.path.startswith('/_nicegui') and request.url.path not in unrestricted_page_routes:
-                        app.storage.user['referrer_path'] = request.url.path  # remember where the user wanted to go
-                        return RedirectResponse('/login')
-                return await call_next(request)
-
-
-        if (open_state["OPEN_IN_WEB"] and args.auth):
-            app.add_middleware(AuthMiddleware)
 
         alljson_list = get_json_list()
         alljson_tab_list = [None for i in alljson_list]
-
-        @ui.page('/login')
-        def login() -> Optional[RedirectResponse]:
-            def try_login() -> None:  # local function to avoid passing username and password as arguments
-                if passwords.get(username.value) == password.value:
-                    app.storage.user.update({'username': username.value, 'authenticated': True})
-                    ui.open('/')  # go back to where the user wanted to go
-                else:
-                    ui.notify('Wrong username or password', color='negative')
-
-            if app.storage.user.get('authenticated', False):
-                return RedirectResponse('/')
-            with ui.card().classes('absolute-center'):
-                username = ui.input('Username').on('keydown.enter', try_login)
-                password = ui.input('Password', password=True, password_toggle_button=True).on('keydown.enter', try_login)
-                ui.button('Log in', on_click=try_login)
-            return None
-
 
         # 如果没有config.json文件且alljson_list长度为0，则创建一个
         if len(alljson_list)==0 and not os.path.exists(os.path.join(MyConfigger.USER_CONFIG_FOLDER, "config.json")):
@@ -141,15 +94,32 @@ if __name__ in {"__main__", "__mp_main__"}:
         # 更新提示
         app.on_connect(check_version)
         updateTextBox = ui.row().style("position: fixed;z-index: 9999;")
-        # Tab栏区域
-        tab_area()
 
+        @ui.refreshable
+        def showContentArea() -> None:
+            """
+            主要内容区域
+            """
+            if args.token and args.token != app.storage.user.get("token"):
+                # token输入页面
+                with ui.card().classes('absolute-center'):
+                    ui.input('Token', password=True, password_toggle_button=True,
+                    on_change=lambda e: [app.storage.user.update({"token": e.value}), showContentArea.refresh() if e.value == args.token else None])
+            else:
+                # Tab栏区域
+                tab_area()
+
+        # 放进页面，提供app.storage.user存储能力
+        @ui.page('/')
+        def MainPage() -> None:
+            showContentArea()
+        
         # 运行GUI
         print(open_state)
         if open_state["OPEN_IN_WEB"]:
-            ui.run(title=f"Blue Archive Aris Helper{MyConfigger.NOWVERSION}", favicon="./DATA/assets/aris.ico", language="zh-cn", reload=False, host=args.host, port=native.find_open_port(), storage_secret=MyConfigger.STORAGE_SECRET)
+            ui.run(title=f"Blue Archive Aris Helper{MyConfigger.NOWVERSION}", favicon="./DATA/assets/aris.ico", language="zh-cn", reload=False, host=args.host, port=args.port, storage_secret=shared_softwareconfig.softwareconfigdict["ENCRYPT_KEY"])
         else:
-            ui.run(native=True, window_size=(1280,720), title=f"Blue Archive Aris Helper{MyConfigger.NOWVERSION}", favicon="./DATA/assets/aris.ico", language="zh-cn", reload=False, host={args.host}, port=native.find_open_port())
+            ui.run(native=True, window_size=(1280,720), title=f"Blue Archive Aris Helper{MyConfigger.NOWVERSION}", favicon="./DATA/assets/aris.ico", language="zh-cn", reload=False, host={args.host}, port=args.port, storage_secret=shared_softwareconfig.softwareconfigdict["ENCRYPT_KEY"])
 
     except Exception as e:
         import traceback
