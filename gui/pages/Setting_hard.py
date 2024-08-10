81a1c7ddb06cf6b5d87e39849dff31d05a59c9bf
Yusen Wu <sanmusen_wu@outlook.com>
2024-08-10 23:33:47 +0100

    Feat: Note in GUI

--- gui/pages/Setting_hard.py
+++ gui/pages/Setting_hard.py
@@ -1,12 +1,16 @@
 from nicegui import ui
 from gui.components.list_edit_area import list_edit_area
 
-def set_hard(config):
+def set_hard(config, shared_softwareconfig):
     with ui.row():
         ui.link_target("HARD")
         ui.label(config.get_text("task_hard")).style('font-size: x-large')
     
     ui.label(config.get_text("config_desc_times"))
+    
+    show_note = {"val": False}
+    ui.switch(config.get_text("button_show")+config.get_text("desc_note")).bind_value(show_note, "val")
+    ui.textarea().bind_value(shared_softwareconfig.softwareconfigdict["NOTE"], "HARD_NOTE").style("width: 70%").bind_visibility_from(show_note, "val")
     
     list_edit_area(
         config.userconfigdict["HARD"], 
