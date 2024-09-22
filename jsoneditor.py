73ed3e24ffa1d8265070c95a4b2cf8824eed138a
Liu Yan <liu5519@icloud.com>
2024-09-23 02:26:44 +0800

    refactor(json_arg_panel): use splitter split that is created by set_xxx func

--- jsoneditor.py
+++ jsoneditor.py
@@ -6,7 +6,7 @@ from nicegui import ui, app
 
 def main():
     app.on_connect(check_version)
-    ui.run(title=f"BAAH{MyConfigger.NOWVERSION}", favicon="./DATA/assets/aris.ico", language="zh-CN", reload=True)
+    ui.run(title=f"BAAH{MyConfigger.NOWVERSION}", favicon="./DATA/assets/aris.ico", language="zh-CN", reload=False)
 
 
 if __name__ in {"__main__", "__mp_main__"}:
