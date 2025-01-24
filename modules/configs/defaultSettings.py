90951825ca778b74e8d581bbbaae39859d502226
yusen <sanmusen_wu@outlook.com>
2025-01-24 10:27:42 +0800

    Feat: can invite n-th stu, and invite delay

--- modules/configs/defaultSettings.py
+++ modules/configs/defaultSettings.py
@@ -232,7 +232,8 @@ defaultUserDict = {
     # 要邀请的学生在momotalk中的序号，从1开始
     "CAFE1_INVITE_SEQ":{"d":1},
     "CAFE2_INVITE_SEQ":{"d":2},
-
+    # 咖啡馆邀请发生同名时是否顺延邀请序号
+    "CAFE_INVITE_SAME_NAME_DELAY":{"d":False},
 }
 
 # 软件的config里的默认值
